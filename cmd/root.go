690131ae70b51b82f62ab662f0b6134b4412763c
Błażej Pachut <blazej.pachut@speednet.pl>
2026-02-17 09:28:46 +0100

    chore: clean up Cobra boilerplate - update descriptions, remove scaffold remnants

--- cmd/root.go
+++ cmd/root.go
@@ -1,18 +1,5 @@
-/*
-Copyright © 2020 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-	http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// Copyright 2020 go-mdp contributors
+// Licensed under the Apache License, Version 2.0
 package cmd
 
 import (
@@ -35,15 +22,8 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "go-mdp",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
+	Short: "MySQL dump anonymizer/processor",
+	Long:  "go-mdp processes MySQL dump files and anonymizes data based on a JSON configuration.",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		input, err := getInputStream(cmd.Flag(FlagNameInput).Value.String())
 		if err != nil {
@@ -169,45 +149,11 @@ var (
 
 func init() {
 	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	//rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.go-mdp.yaml)")
 	rootCmd.Flags().StringP(FlagNameInput, "i", "", "input file (if not set stdin is used)")
 	rootCmd.Flags().StringP(FlagNameOutput, "o", "", "output file (if not set stdout is used)")
 	rootCmd.Flags().StringP(FlagNameConfig, "c", "config.json", "config file")
 	rootCmd.Flags().StringP(FlagNameConfigData, "f", "", "encoded config file")
 	rootCmd.Flags().BoolP(FlagNameConfigIsZipped, "z", false, "is encoded config data zipped?")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	//if cfgFile != "" {
-	//  // Use config file from the flag.
-	//  viper.SetConfigFile(cfgFile)
-	//} else {
-	//  // Find home directory.
-	//  home, err := homedir.Dir()
-	//  if err != nil {
-	//    fmt.Println(err)
-	//    os.Exit(1)
-	//  }
-	//
-	//  // Search config in home directory with name ".go-mdp" (without extension).
-	//  viper.AddConfigPath(home)
-	//  viper.SetConfigName(".go-mdp")
-	//}
-	//
-	//viper.AutomaticEnv() // read in environment variables that match
-	//
-	//// If a config file is found, read it in.
-	//if err := viper.ReadInConfig(); err == nil {
-	//  fmt.Println("Using config file:", viper.ConfigFileUsed())
-	//}
-}
+func initConfig() {}
